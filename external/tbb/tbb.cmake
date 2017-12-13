3d7d11bcec7b02bc7ee18db8cba02fd4d5bd30c9
Mathijs Molenaar <mathijs.l.molenaar@gmail.com>
2017-12-13 04:24:52 -0800

    Build TBB from source on all non-windows platforms (both Linux and macOS have GNU Make?)

--- external/tbb/tbb.cmake
+++ external/tbb/tbb.cmake
@@ -1,29 +1,26 @@
+# If on Windows, use precompiled binaries because TBB uses GNU Make as build system (welcome to 2017/2018 Intel...)
 if (WIN32)
-set(TBB_PLATFORM_POSTFIX "win")
-elseif (APPLE)
-set(TBB_PLATFORM_POSTFIX "mac")
-else()# Assume Linux
-set(TBB_PLATFORM_POSTFIX "lin")
-endif()
-set(TBB_PATH "${CMAKE_CURRENT_LIST_DIR}/tbb2018_20171205oss_${TBB_PLATFORM_POSTFIX}/tbb2018_20171205oss/")
+
+set(TBB_PATH "${CMAKE_CURRENT_LIST_DIR}/tbb2018_20171205oss_win/tbb2018_20171205oss/")
 set(CMAKE_PREFIX_PATH "${CMAKE_PREFIX_PATH};${TBB_PATH}")
-find_package(TBB)
+find_package(TBB REQUIRED tbb)
 
-message("TBB BIN DIR: ${TBB_BINDIR}")
-if (WIN32)
-
+# Copy the DLL files to the binary folder
 if (CMAKE_SIZEOF_VOID_P EQUAL 4)
 file(COPY "${TBB_PATH}/bin/ia32/vc14/" DESTINATION ${CMAKE_BINARY_DIR})
 else ()
 file(COPY "${TBB_PATH}/bin/intel64/vc14/" DESTINATION ${CMAKE_BINARY_DIR})
 endif ()
 
-elseif (NOT APPLE)# Must be Linux?
+else ()# Must be Linux?
 
-if (CMAKE_SIZEOF_VOID_P EQUAL 4)
-file(COPY "${TBB_PATH}/bin/ia32/gcc4.7/" DESTINATION ${CMAKE_BINARY_DIR})
-else ()
-file(COPY "${TBB_PATH}/bin/intel64/gcc4.7/" DESTINATION ${CMAKE_BINARY_DIR})
-endif ()
+include("${CMAKE_CURRENT_LIST_DIR}/tbb/cmake/TBBBuild.cmake")
+tbb_build(TBB_ROOT "${CMAKE_CURRENT_LIST_DIR}/tbb/" CONFIG_DIR TBB_DIR)
+find_package(TBB REQUIRED tbb)
+#if (CMAKE_SIZEOF_VOID_P EQUAL 4)
+#file(COPY "${TBB_PATH}/bin/ia32/gcc4.7/" DESTINATION ${CMAKE_BINARY_DIR})
+#else ()
+#file(COPY "${TBB_PATH}/bin/intel64/gcc4.7/" DESTINATION ${CMAKE_BINARY_DIR})
+#endif ()
 
 endif()
