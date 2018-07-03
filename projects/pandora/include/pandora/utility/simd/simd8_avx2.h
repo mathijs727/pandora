4b4905bacccfaf4fe966a66ca01a28276c7eb698
mathijs727 <mathijs.l.molenaar@gmail.com>
2018-07-03 17:36:48 +0200

    Use lookup table for AVX2 vector compression

--- projects/pandora/include/pandora/utility/simd/simd8_avx2.h
+++ projects/pandora/include/pandora/utility/simd/simd8_avx2.h
@@ -1,7 +1,26 @@
 #pragma once
+#include <array>
 #include <cstdint>
 #include <gsl/span>
 #include <immintrin.h> // AVX / AVX2
+
+static constexpr std::array<uint64_t, 256> genCompressLUT()
+{
+    std::array<uint64_t, 256> result = {};
+    for (uint64_t mask = 0; mask < 256; mask++) {
+        uint64_t indices = 0; // Permutation indices
+        uint64_t k = 0;
+        for (uint64_t bit = 0; bit < 8; bit++) {
+            if ((mask & (1ull << bit)) != 0) { // If bit is set
+                indices |= bit << k; // Add to permutation indices
+                k += 8; // One byte
+            }
+        }
+        result[mask] = indices;
+    }
+    return result;
+}
+static constexpr std::array<uint64_t, 256> s_indicesLUT = genCompressLUT();
 
 template <typename T, int S>
 class vec8;
@@ -15,12 +34,12 @@ public:
     //mask8() = default;
     inline mask8(const __m256i& value)
         : m_value(value)
-        , m_mask(_mm256_movemask_ps(_mm256_castsi256_ps(m_value)))
+        , m_bitMask(_mm256_movemask_ps(_mm256_castsi256_ps(m_value)))
     {
     }
     inline mask8(const __m256& value)
         : m_value(_mm256_castps_si256(value))
-        , m_mask(_mm256_movemask_ps(value))
+        , m_bitMask(_mm256_movemask_ps(value))
     {
     }
     inline mask8(bool v0, bool v1, bool v2, bool v3, bool v4, bool v5, bool v6, bool v7)
@@ -34,7 +53,7 @@ public:
             v2 ? 0xFFFFFFFF : 0x0,
             v1 ? 0xFFFFFFFF : 0x0,
             v0 ? 0xFFFFFFFF : 0x0);
-		m_mask = _mm256_movemask_ps(_mm256_castsi256_ps(m_value));
+        m_bitMask = _mm256_movemask_ps(_mm256_castsi256_ps(m_value));
     }
 
     inline int count()
@@ -45,7 +64,7 @@ public:
 
 private:
     __m256i m_value;
-    unsigned m_mask;
+    unsigned m_bitMask;
 
     template <typename T, int S>
     friend class vec8; // Not possible to friend only vec8_base<T, 1>
@@ -69,10 +88,10 @@ public:
     {
         m_value = _mm256_set_epi32(v7, v6, v5, v4, v3, v2, v1, v0);
     }
-	inline vec8(__m256i value)
-		: m_value(value)
-	{
-	}
+    inline vec8(__m256i value)
+        : m_value(value)
+    {
+    }
 
     inline void load(gsl::span<const uint32_t, 8> v)
     {
@@ -119,13 +138,13 @@ public:
 
     inline vec8<uint32_t, 8> operator>>(const vec8<uint32_t, 8>& amount) const
     {
-		return vec8(_mm256_srlv_epi32(m_value, amount.m_value));
+        return vec8(_mm256_srlv_epi32(m_value, amount.m_value));
     }
 
-	inline vec8<uint32_t, 8> operator&(const vec8<uint32_t, 8>& other) const
-	{
-		return vec8(_mm256_and_si256(m_value, other.m_value));
-	}
+    inline vec8<uint32_t, 8> operator&(const vec8<uint32_t, 8>& other) const
+    {
+        return vec8(_mm256_and_si256(m_value, other.m_value));
+    }
 
     inline mask8<8> operator<(const vec8<uint32_t, 8>& other) const
     {
@@ -139,22 +158,21 @@ public:
 
     inline vec8<uint32_t, 8> permute(const vec8<uint32_t, 8>& index) const
     {
-		return vec8(_mm256_permutevar8x32_epi32(m_value, index.m_value));
+        return vec8(_mm256_permutevar8x32_epi32(m_value, index.m_value));
     }
 
     inline vec8<uint32_t, 8> compress(const mask8<8>& mask) const
     {
-        // TODO: paper uses LUT from compressed map -> permutation vector
-        // Check if that performs better than this implementation
+        uint64_t wantedIndices = s_indicesLUT[mask.m_bitMask];
 
-        // https://stackoverflow.com/questions/36932240/avx2-what-is-the-most-efficient-way-to-pack-left-based-on-a-mask/36951611
+        /*// https://stackoverflow.com/questions/36932240/avx2-what-is-the-most-efficient-way-to-pack-left-based-on-a-mask/36951611
         // Emulate compress operation since it is not part of AVX2 (only AVX512)
-        uint64_t expandedMask = _pdep_u64(mask.m_mask, 0x0101010101010101); // Unpack each bit to a byte
+        uint64_t expandedMask = _pdep_u64(mask.m_bitMask, 0x0101010101010101); // Unpack each bit to a byte
         expandedMask *= 0xFF; // mask |= mask<<1 | mask<<2 | ... | mask<<7;
         // ABC... -> AAAAAAAABBBBBBBBCCCCCCCC...: replicate each bit to fill its byte
 
         const uint64_t identityIndices = 0x0706050403020100; // The identity shuffle for vpermps, packed to one index per byte
-        uint64_t wantedIndices = _pext_u64(identityIndices, expandedMask);
+        uint64_t wantedIndices = _pext_u64(identityIndices, expandedMask);*/
 
         __m128i byteVec = _mm_cvtsi64_si128(wantedIndices);
         __m256i shuffleMask = _mm256_cvtepu8_epi32(byteVec);
@@ -184,10 +202,10 @@ public:
     {
         m_value = _mm256_set_ps(v7, v6, v5, v4, v3, v2, v1, v0);
     }
-	inline vec8(__m256 value)
-		: m_value(value)
-	{
-	}
+    inline vec8(__m256 value)
+        : m_value(value)
+    {
+    }
 
     inline void load(gsl::span<const float, 8> v)
     {
@@ -217,17 +235,17 @@ public:
 
     inline vec8<float, 8> operator-(const vec8<float, 8>& other) const
     {
-		return vec8(_mm256_sub_ps(m_value, other.m_value));
+        return vec8(_mm256_sub_ps(m_value, other.m_value));
     }
 
     inline vec8<float, 8> operator*(const vec8<float, 8>& other) const
     {
-		return vec8(_mm256_mul_ps(m_value, other.m_value));
+        return vec8(_mm256_mul_ps(m_value, other.m_value));
     }
 
     inline vec8<float, 8> operator/(const vec8<float, 8>& other) const
     {
-		return vec8(_mm256_div_ps(m_value, other.m_value));
+        return vec8(_mm256_div_ps(m_value, other.m_value));
     }
 
     inline mask8<8> operator<(const vec8<float, 8>& other) const
@@ -242,27 +260,26 @@ public:
 
     inline vec8<float, 8> permute(const vec8<uint32_t, 8>& index) const
     {
-		return vec8(_mm256_permutevar8x32_ps(m_value, index.m_value));
+        return vec8(_mm256_permutevar8x32_ps(m_value, index.m_value));
     }
 
-	inline vec8<float, 8> compress(const mask8<8>& mask) const
-	{
-		// TODO: paper uses LUT from compressed map -> permutation vector
-		// Check if that performs better than this implementation
+    inline vec8<float, 8> compress(const mask8<8>& mask) const
+    {
+        uint64_t wantedIndices = s_indicesLUT[mask.m_bitMask];
 
-		// https://stackoverflow.com/questions/36932240/avx2-what-is-the-most-efficient-way-to-pack-left-based-on-a-mask/36951611
+        /*// https://stackoverflow.com/questions/36932240/avx2-what-is-the-most-efficient-way-to-pack-left-based-on-a-mask/36951611
 		// Emulate compress operation since it is not part of AVX2 (only AVX512)
-		uint64_t expandedMask = _pdep_u64(mask.m_mask, 0x0101010101010101); // Unpack each bit to a byte
+		uint64_t expandedMask = _pdep_u64(mask.m_bitMask, 0x0101010101010101); // Unpack each bit to a byte
 		expandedMask *= 0xFF; // mask |= mask<<1 | mask<<2 | ... | mask<<7;
 							  // ABC... -> AAAAAAAABBBBBBBBCCCCCCCC...: replicate each bit to fill its byte
 
 		const uint64_t identityIndices = 0x0706050403020100; // The identity shuffle for vpermps, packed to one index per byte
-		uint64_t wantedIndices = _pext_u64(identityIndices, expandedMask);
+		uint64_t wantedIndices = _pext_u64(identityIndices, expandedMask);*/
 
-		__m128i byteVec = _mm_cvtsi64_si128(wantedIndices);
-		__m256i shuffleMask = _mm256_cvtepu8_epi32(byteVec);
-		return vec8(_mm256_permutevar8x32_ps(m_value, shuffleMask));
-	}
+        __m128i byteVec = _mm_cvtsi64_si128(wantedIndices);
+        __m256i shuffleMask = _mm256_cvtepu8_epi32(byteVec);
+        return vec8(_mm256_permutevar8x32_ps(m_value, shuffleMask));
+    }
 
     friend vec8<float, 8> min(const vec8<float, 8>& a, const vec8<float, 8>& b);
     friend vec8<float, 8> max(const vec8<float, 8>& a, const vec8<float, 8>& b);
