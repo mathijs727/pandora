c2dae9d00686cde4d0dcb751cf45269c9753d1dd
mathijs727 <mathijs.l.molenaar@gmail.com>
2018-07-31 12:42:55 +0200

    Remove preprocess step to improve performance
    Removing the large std::vectors improves performance a lot

--- projects/pandora/src/svo/mesh_to_voxel.cpp
+++ projects/pandora/src/svo/mesh_to_voxel.cpp
@@ -22,77 +22,6 @@ void meshToVoxelGridNaive(VoxelGrid& voxelGrid, const Bounds& gridBounds, const 
     auto triangles = mesh.getTriangles();
     auto positions = mesh.getPositions();
 
-    size_t N = triangles.size();
-
-    std::vector<glm::vec3> tBoundsMins(N); // Min corner of triangle bounds
-    std::vector<glm::vec3> tBoundsExtents(N); // Extent of triangle bounds
-    std::vector<glm::vec3> ns(N); // Normals
-
-    // Used for 3D AABB / plane overlap test
-    std::vector<float> d1s(N);
-    std::vector<float> d2s(N);
-
-    // Used for 2D triangle / AABB overlap test
-    std::vector<glm::vec2> n_xy_es[3] = { std::vector<glm::vec2>(N), std::vector<glm::vec2>(N), std::vector<glm::vec2>(N) };
-    std::vector<float> d_xy_es[3] = { std::vector<float>(N), std::vector<float>(N), std::vector<float>(N) };
-    std::vector<glm::vec2> n_zx_es[3] = { std::vector<glm::vec2>(N), std::vector<glm::vec2>(N), std::vector<glm::vec2>(N) };
-    std::vector<float> d_zx_es[3] = { std::vector<float>(N), std::vector<float>(N), std::vector<float>(N) };
-    std::vector<glm::vec2> n_yz_es[3] = { std::vector<glm::vec2>(N), std::vector<glm::vec2>(N), std::vector<glm::vec2>(N) };
-    std::vector<float> d_yz_es[3] = { std::vector<float>(N), std::vector<float>(N), std::vector<float>(N) };
-
-    for (int t = 0; t < triangles.size(); t++) {
-        const auto& triangle = triangles[t];
-        glm::vec3 v[3] = { positions[triangle[0]], positions[triangle[1]], positions[triangle[2]] };
-        glm::vec3 e[3] = { v[1] - v[0], v[2] - v[1], v[0] - v[2] };
-
-        // Triangle bounds
-        glm::vec3 tBoundsMin = glm::min(v[0], glm::min(v[1], v[2]));
-        glm::vec3 tBoundsMax = glm::max(v[0], glm::max(v[1], v[2]));
-        tBoundsMins[t] = tBoundsMin;
-        tBoundsExtents[t] = tBoundsMax - tBoundsMin;
-
-        // Normal
-        glm::vec3 n = glm::normalize(glm::cross(e[0], e[1]));
-        ns[t] = n;
-
-        // Critical point
-        glm::vec3 c(
-            n.x > 0 ? delta_p.x : 0,
-            n.y > 0 ? delta_p.y : 0,
-            n.z > 0 ? delta_p.z : 0);
-        d1s[t] = glm::dot(n, c - v[0]);
-        d2s[t] = glm::dot(n, (delta_p - c) - v[0]);
-
-        for (int i = 0; i < 3; i++) { // For each edge
-            {
-                glm::vec2 n_xy_ei = glm::vec2(-e[i].y, e[i].x) * (n.z >= 0 ? 1.0f : -1.0f);
-                glm::vec2 v_xy_i(v[i].x, v[i].y);
-                float d_xy_ei = -glm::dot(n_xy_ei, v_xy_i) + std::max(0.0f, delta_p.x * n_xy_ei.x) + std::max(0.0f, delta_p.y * n_xy_ei.y);
-
-                n_xy_es[i][t] = n_xy_ei;
-                d_xy_es[i][t] = d_xy_ei;
-            }
-
-            {
-                glm::vec2 n_zx_ei = glm::vec2(-e[i].x, e[i].z) * (n.y >= 0 ? 1.0f : -1.0f);
-                glm::vec2 v_zx_i(v[i].z, v[i].x);
-                float d_zx_ei = -glm::dot(n_zx_ei, v_zx_i) + std::max(0.0f, delta_p.z * n_zx_ei.x) + std::max(0.0f, delta_p.x * n_zx_ei.y);
-
-                n_zx_es[i][t] = n_zx_ei;
-                d_zx_es[i][t] = d_zx_ei;
-            }
-
-            {
-                glm::vec2 n_yz_ei = glm::vec2(-e[i].z, e[i].y) * (n.x >= 0 ? 1.0f : -1.0f);
-                glm::vec2 v_yz_i(v[i].y, v[i].z);
-                float d_yz_ei = -glm::dot(n_yz_ei, v_yz_i) + std::max(0.0f, delta_p.y * n_yz_ei.x) + std::max(0.0f, delta_p.z * n_yz_ei.y);
-
-                n_yz_es[i][t] = n_yz_ei;
-                d_yz_es[i][t] = d_yz_ei;
-            }
-        }
-    }
-
     // ==================== Intersect phase ====================
     const glm::vec3 worldToVoxelScale = glm::vec3(voxelGrid.resolution()) / scale;
     const glm::vec3 voxelToWorldScale = scale / glm::vec3(voxelGrid.resolution());
@@ -102,14 +31,26 @@ void meshToVoxelGridNaive(VoxelGrid& voxelGrid, const Bounds& gridBounds, const 
     const glm::ivec3 maxGridVoxel(voxelGrid.resolution() - 1);
 
 	for (int t = 0; t < triangles.size(); t++) {
-        glm::vec3 tBoundsMin = tBoundsMins[t];
-        glm::vec3 tBoundsExtent = tBoundsExtents[t];
+		const auto& triangle = triangles[t];
+		glm::vec3 v[3] = { positions[triangle[0]], positions[triangle[1]], positions[triangle[2]] };
+		glm::vec3 e[3] = { v[1] - v[0], v[2] - v[1], v[0] - v[2] };
+		glm::vec3 n = glm::cross(e[0], e[1]);
+
+		// Triangle bounds
+		glm::vec3 tBoundsMin = glm::min(v[0], glm::min(v[1], v[2]));
+		glm::vec3 tBoundsMax = glm::max(v[0], glm::max(v[1], v[2]));
+		glm::vec3 tBoundsExtent = tBoundsMax - tBoundsMin;
 
         glm::ivec3 tBoundsMinVoxel = glm::min(worldToVoxel(tBoundsMin), maxGridVoxel);// Fix for triangles on the border of the voxel grid
         glm::ivec3 tBoundsMaxVoxel = worldToVoxel(tBoundsMin + tBoundsExtent); // Upper bound
 		
-		//tBoundsMinVoxel = glm::ivec3(0);
-		//tBoundsMaxVoxel = maxGridVoxel; // Upper bound
+																			   // Critical point
+		glm::vec3 c(
+			n.x > 0 ? delta_p.x : 0,
+			n.y > 0 ? delta_p.y : 0,
+			n.z > 0 ? delta_p.z : 0);
+		float d1 = glm::dot(n, c - v[0]);
+		float d2 = glm::dot(n, (delta_p - c) - v[0]);
 
         // For each voxel in the triangles AABB
         for (int z = tBoundsMinVoxel.z; z <= std::min(tBoundsMaxVoxel.z, maxGridVoxel.z); z++) {
@@ -118,21 +59,15 @@ void meshToVoxelGridNaive(VoxelGrid& voxelGrid, const Bounds& gridBounds, const 
                     // Intersection test
                     glm::vec3 p = voxelToWorld(glm::ivec3(x, y, z));
 
-                    bool planeIntersect = ((glm::dot(ns[t], p) + d1s[t]) * (glm::dot(ns[t], p) + d2s[t])) <= 0;
+
+                    bool planeIntersect = ((glm::dot(n, p) + d1) * (glm::dot(n, p) + d2)) <= 0;
                     if (!planeIntersect)
                         continue;
 
 					bool triangleIntersect2D = true;
                     for (int i = 0; i < 3; i++) {
-                        //triangleIntersect2D &= (glm::dot(n_xy_es[i][t], glm::vec2(p.x, p.y)) + d_xy_es[i][t]) >= 0;
-						//triangleIntersect2D &= (glm::dot(n_zx_es[i][t], glm::vec2(p.z, p.x)) + d_zx_es[i][t]) >= 0;
-                        //triangleIntersect2D &= (glm::dot(n_yz_es[i][t], glm::vec2(p.y, p.z)) + d_yz_es[i][t]) >= 0;
-
 						// Equivalent in-line version
-						const auto& triangle = triangles[t];
-						glm::vec3 v[3] = { positions[triangle[0]], positions[triangle[1]], positions[triangle[2]] };
-						glm::vec3 e[3] = { v[1] - v[0], v[2] - v[1], v[0] - v[2] };
-						glm::vec3 n = glm::cross(e[0], e[1]);
+						
 						{
 							glm::vec2 n_xy_ei = glm::vec2(-e[i].y, e[i].x) * (n.z >= 0 ? 1.0f : -1.0f);
 							glm::vec2 v_xy_i(v[i].x, v[i].y);
--- projects/voxel/src/main.cpp
+++ projects/voxel/src/main.cpp
@@ -1,15 +1,16 @@
+#include "pandora/geometry/triangle.h"
+#include "pandora/svo/mesh_to_voxel.h"
+#include "pandora/svo/voxel_grid.h"
 #include <array>
-#include <tinyply.h>
+#include <cassert>
+#include <chrono>
+#include <fstream>
 #include <glm/glm.hpp>
 #include <gsl/gsl>
 #include <iostream>
-#include <string_view>
-#include <fstream>
 #include <string>
-#include <cassert>
-#include "pandora/svo/voxel_grid.h"
-#include "pandora/svo/mesh_to_voxel.h"
-#include "pandora/geometry/triangle.h"
+#include <string_view>
+#include <tinyply.h>
 
 using namespace std::string_literals;
 using namespace tinyply;
@@ -17,39 +18,46 @@ using namespace pandora;
 
 void exportMesh(gsl::span<glm::vec3> vertices, gsl::span<glm::ivec3> triangles, std::string_view filePath)
 {
-	assert(vertices.size() > 0 && triangles.size() > 0);
+    assert(vertices.size() > 0 && triangles.size() > 0);
 
-	std::filebuf fb;
-	fb.open(std::string(filePath), std::ios::out | std::ios::binary);
-	std::ostream outStream(&fb);
-	if (outStream.fail()) throw std::runtime_error("failed to open "s + std::string(filePath));
+    std::filebuf fb;
+    fb.open(std::string(filePath), std::ios::out | std::ios::binary);
+    std::ostream outStream(&fb);
+    if (outStream.fail())
+        throw std::runtime_error("failed to open "s + std::string(filePath));
 
-	PlyFile plyFile;
-	plyFile.add_properties_to_element("vertex", { "x", "y", "z" },
-		Type::FLOAT32, vertices.size(), reinterpret_cast<uint8_t*>(vertices.data()), Type::INVALID, 0);
-	plyFile.add_properties_to_element("face", { "vertex_indices" },
-		Type::INT32, triangles.size(), reinterpret_cast<uint8_t*>(triangles.data()), Type::UINT8, 3);
+    PlyFile plyFile;
+    plyFile.add_properties_to_element("vertex", { "x", "y", "z" },
+        Type::FLOAT32, vertices.size(), reinterpret_cast<uint8_t*>(vertices.data()), Type::INVALID, 0);
+    plyFile.add_properties_to_element("face", { "vertex_indices" },
+        Type::INT32, triangles.size(), reinterpret_cast<uint8_t*>(triangles.data()), Type::UINT8, 3);
 
-	plyFile.write(outStream, false); // write ascii
-	fb.close();
+    plyFile.write(outStream, false); // write ascii
+    fb.close();
 }
 
 int main()
 {
-	const std::string projectBasePath = "../../"s;
-	auto meshes = TriangleMesh::loadFromFile(projectBasePath + "assets/3dmodels/stanford/bun_zipper.ply");
-	
-	Bounds gridBounds;
-	for (const auto& mesh : meshes)
-		gridBounds.extend(mesh->getBounds());
+    const std::string projectBasePath = "../../"s;
+    auto meshes = TriangleMesh::loadFromFile(projectBasePath + "assets/3dmodels/stanford/dragon_vrip.ply");
 
-	int resolution = 128;
-	VoxelGrid voxelGrid(resolution);
-	for (const auto& mesh : meshes)
-		meshToVoxelGridNaive(voxelGrid, gridBounds, *mesh);
+    Bounds gridBounds;
+    for (const auto& mesh : meshes)
+        gridBounds.extend(mesh->getBounds());
 
-	auto [vertices, triangles] = voxelGrid.generateSurfaceMesh();
-	exportMesh(vertices, triangles, "hello_world.ply");
+    int resolution = 128;
+    VoxelGrid voxelGrid(resolution);
+
+	using clock = std::chrono::high_resolution_clock;
+	auto start = clock::now();
+    for (const auto& mesh : meshes)
+        meshToVoxelGridNaive(voxelGrid, gridBounds, *mesh);
+	auto end = clock::now();
+	auto timeDelta = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+	std::cout << "Time to voxelize: " << timeDelta.count() / 1000.0f << "ms" << std::endl;
+
+    auto [vertices, triangles] = voxelGrid.generateSurfaceMesh();
+    exportMesh(vertices, triangles, "hello_world.ply");
 
     std::cout << "HELLO WORLD!" << std::endl;
     return 0;
