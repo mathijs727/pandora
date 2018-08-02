31c7a66c953e63c5a16f761d62026bd76f56e138
mathijs727 <mathijs.l.molenaar@gmail.com>
2018-08-02 23:00:45 +0200

    Fix bug in int to float conversion and set stack depth to 23 like in the reference code

--- projects/pandora/src/svo/sparse_voxel_octree.cpp
+++ projects/pandora/src/svo/sparse_voxel_octree.cpp
@@ -66,7 +66,7 @@ static int floatAsInt(float v)
     return r;
 }
 
-static int intAsFloat(int v)
+static float intAsFloat(int v)
 {
     static_assert(sizeof(float) == sizeof(int));
 
@@ -79,27 +79,28 @@ static int intAsFloat(int v)
 
 std::optional<float> SparseVoxelOctree::intersect(Ray ray)
 {
-	if (ray.tfar == std::numeric_limits<float>::max())
-		ray.direction *= 1000.0f;
-	else
-		ray.direction *= ray.tfar;
-
+    if (ray.tfar == std::numeric_limits<float>::max())
+        ray.direction *= 1000.0f;
+    else
+        ray.direction *= ray.tfar;
 
     // Based on the reference implementation of Efficient Sparse Voxel Octrees:
     // https://github.com/poelzi/efficient-sparse-voxel-octrees/blob/master/src/octree/cuda/Raycast.inl
-    const int CAST_STACK_DEPTH = intLog2(m_resolution);
+    const int CAST_STACK_DEPTH = 23; //intLog2(m_resolution);
 
-	// Get rid of small ray direction components to avoid division by zero
-	const float epsilon = std::exp2f(-CAST_STACK_DEPTH);
-	if (fabsf(ray.direction.x) < epsilon) ray.direction.x = copysignf(epsilon, ray.direction.x);
-	if (fabsf(ray.direction.y) < epsilon) ray.direction.y = copysignf(epsilon, ray.direction.y);
-	if (fabsf(ray.direction.z) < epsilon) ray.direction.z = copysignf(epsilon, ray.direction.z);
+    // Get rid of small ray direction components to avoid division by zero
+    const float epsilon = std::exp2f(-CAST_STACK_DEPTH);
+    if (fabsf(ray.direction.x) < epsilon)
+        ray.direction.x = copysignf(epsilon, ray.direction.x);
+    if (fabsf(ray.direction.y) < epsilon)
+        ray.direction.y = copysignf(epsilon, ray.direction.y);
+    if (fabsf(ray.direction.z) < epsilon)
+        ray.direction.z = copysignf(epsilon, ray.direction.z);
 
     // Precompute the coefficients of tx(x), ty(y) and tz(z).
     // The octree is assumed to reside at coordinates [1, 2].
     glm::vec3 tCoef = 1.0f / -glm::abs(ray.direction);
     glm::vec3 tBias = tCoef * ray.origin;
-
 
     // Select octant mask to mirro the coordinate system so taht ray direction is negative along each axis
     uint32_t octantMask = 7;
@@ -129,6 +130,9 @@ std::optional<float> SparseVoxelOctree::intersect(Ray ray)
     tMin = std::max(tMin, 0.0f);
     //tMax = std::min(tMax, 1.0f);
 
+    if (tMin >= tMax)
+        return {};
+
     // Intersection of ray (negative in all directions) with the root node (cube at [1, 2])
     if (1.5 * tCoef.x - tBias.x > tMin) {
         idx ^= (1 << 0);
@@ -144,12 +148,12 @@ std::optional<float> SparseVoxelOctree::intersect(Ray ray)
     }
 
     // Traverse voxels along the ray as long as the current voxel stays within the octree
-    ALWAYS_ASSERT(CAST_STACK_DEPTH < 16, "SVO too deep to traverse with a stack of size 16. Increase the stack size or use a shallower SVO.");
+    //ALWAYS_ASSERT(CAST_STACK_DEPTH < 16, "SVO too deep to traverse with a stack of size 16. Increase the stack size or use a shallower SVO.");
     struct StackItem {
         ChildDescriptor parent;
         float tMax;
     };
-    std::array<StackItem, 16> stack;
+    std::array<StackItem, CAST_STACK_DEPTH + 1> stack;
     while (scale < CAST_STACK_DEPTH) {
         // === INTERSECT ===
         // Determine the maximum t-value of the cube by evaluating tx(), ty() and tz() at its corner
@@ -179,6 +183,8 @@ std::optional<float> SparseVoxelOctree::intersect(Ray ray)
                 idx = 0;
                 scale--;
                 scaleExp2 = half;
+				if (scaleExp2 == 0.0f)
+					std::cout << "Z" << std::endl;
                 if (tCenter.x > tMin) {
                     idx ^= (1 << 0);
                     pos.x += scaleExp2;
@@ -234,41 +240,43 @@ std::optional<float> SparseVoxelOctree::intersect(Ray ray)
             if ((stepMask & (1 << 2)) != 0) {
                 differingBits |= floatAsInt(pos.z) ^ floatAsInt(pos.z + scaleExp2);
             }
-            scale = (floatAsInt((float)differingBits) >> 23) - 127; // Position of the highest bit
-            scaleExp2 = intAsFloat((scale - CAST_STACK_DEPTH + 127) << 23); // exp2f(scale - s_max)
+            scale = (floatAsInt((float)differingBits) >> 23) - 127; // Position of the highest bit (complicated alternative to bitscan)
+            if (scale == -127)
+                std::cout << "X" << std::endl;
+			float newVal = intAsFloat((scale - CAST_STACK_DEPTH + 127) << 23);
+            if (newVal == 0.0f)
+                std::cout << "Y" << std::endl;
+            scaleExp2 = newVal; // exp2f(scale - s_max)
 
-			if (scale > CAST_STACK_DEPTH)
-				return {};
+            // Restore parent voxel from the stack
+            parent = stack[scale].parent;
+            tMax = stack[scale].tMax;
 
-			// Restore parent voxel from the stack
-			parent = stack[scale].parent;
-			tMax = stack[scale].tMax;
-
-			// Round cube position and extract child slot index
-			int shx = floatAsInt(pos.x) >> scale;
-			int shy = floatAsInt(pos.y) >> scale;
-			int shz = floatAsInt(pos.z) >> scale;
-			pos.x = intAsFloat(shx << scale);
-			pos.y = intAsFloat(shy << scale);
-			pos.z = intAsFloat(shz << scale);
-			idx = (shx & 1) | ((shy & 1) << 1) | ((shz & 1) << 2);
+            // Round cube position and extract child slot index
+            int shx = floatAsInt(pos.x) >> scale;
+            int shy = floatAsInt(pos.y) >> scale;
+            int shz = floatAsInt(pos.z) >> scale;
+            pos.x = intAsFloat(shx << scale);
+            pos.y = intAsFloat(shy << scale);
+            pos.z = intAsFloat(shz << scale);
+            idx = (shx & 1) | ((shy & 1) << 1) | ((shz & 1) << 2);
         }
     }
 
-	// Indicate miss if we are outside the octree
-	if (scale >= CAST_STACK_DEPTH)
-		tMin = 2.0f;
+    // Indicate miss if we are outside the octree
+    if (scale >= CAST_STACK_DEPTH)
+        tMin = 2.0f;
 
-	// Undo mirroring of the coordinate system
-	if ((octantMask & (1 << 0)) == 0)
-		pos.x = 3.0f - scaleExp2 - pos.x;
-	if ((octantMask & (1 << 1)) == 0)
-		pos.y = 3.0f - scaleExp2 - pos.y;
-	if ((octantMask & (1 << 2)) == 0)
-		pos.z = 3.0f - scaleExp2 - pos.z;
+    // Undo mirroring of the coordinate system
+    if ((octantMask & (1 << 0)) == 0)
+        pos.x = 3.0f - scaleExp2 - pos.x;
+    if ((octantMask & (1 << 1)) == 0)
+        pos.y = 3.0f - scaleExp2 - pos.y;
+    if ((octantMask & (1 << 2)) == 0)
+        pos.z = 3.0f - scaleExp2 - pos.z;
 
-	// Output result
-	return tMin;
+    // Output result
+    return tMin;
 }
 
 SparseVoxelOctree::ChildDescriptor SparseVoxelOctree::getChild(const ChildDescriptor& descriptor, int idx) const
@@ -281,7 +289,7 @@ SparseVoxelOctree::ChildDescriptor SparseVoxelOctree::getChild(const ChildDescri
             activeChildCount++;
         }
     }
-	return m_allocator[descriptor.childPtr + activeChildCount];
+    return m_allocator[descriptor.childPtr + activeChildCount];
 }
 
 SparseVoxelOctree::ChildDescriptor SparseVoxelOctree::createStagingDescriptor(gsl::span<bool, 8> validMask, gsl::span<bool, 8> leafMask)
