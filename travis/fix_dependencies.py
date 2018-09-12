ecddb7ec3553cecdec82f44a86b9d68f70dd65cf
mathijs727 <mathijs.l.molenaar@gmail.com>
2018-09-12 16:11:41 +0200

    tinyply fix now does not require Python 3.5
    Travis pls update your ubuntu version

--- travis/fix_dependencies.py
+++ travis/fix_dependencies.py
@@ -1,6 +1,26 @@
 import subprocess
 import os
 
+# https://stackoverflow.com/questions/40590192/getting-an-error-attributeerror-module-object-has-no-attribute-run-while
+def run(*popenargs, input=None, check=False, **kwargs):
+	if input is not None:
+		if 'stdin' in kwargs:
+			raise ValueError('stdin and input arguments may not both be used.')
+		kwargs['stdin'] = subprocess.PIPE
+
+	process = subprocess.Popen(*popenargs, **kwargs)
+	try:
+		stdout, stderr = process.communicate(input)
+	except:
+		process.kill()
+		process.wait()
+		raise
+	retcode = process.poll()
+	if check and retcode:
+		raise subprocess.CalledProcessError(
+			retcode, process.args, output=stdout, stderr=stderr)
+	return retcode, stdout, stderr
+
 def fix_eastl_compiler_traits():
 	filename = "./third_party/eastl/test/packages/EABase/include/Common/EABase/config/eacompilertraits.h"
 	with open(filename, "r") as f:
@@ -19,7 +39,7 @@ def fix_tinyply():
 	tinyply_path = os.path.abspath("./third_party/tinyply/")
 	patch_path = os.path.abspath("./travis/tinyply_fix.patch")
 
-	subprocess.run(["git", "apply", patch_path], cwd=tinyply_path)
+	run(["git", "apply", patch_path], cwd=tinyply_path)
 
 if __name__ == "__main__":
 	#fix_eastl()
