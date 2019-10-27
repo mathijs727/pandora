49837f910518bf0842fc1025c63b5b26eec458f8
mathijs727 <mathijs.l.molenaar@gmail.com>
2019-10-27 09:44:31 +0100

    Minor performance improvements to SSE implementation of crack_atof

--- projects/pbrt_importer/src/crack_atof_sse.cpp
+++ projects/pbrt_importer/src/crack_atof_sse.cpp
@@ -59,22 +59,26 @@ float crack_atof_sse(std::string_view string)
 
     const __m128i pointMask = _mm_set1_epi8('.'); //_mm_set_epi8('.', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e');
     const int pointOffset = std::min(_mm_cmpistri(raw_chars_epi8, pointMask, _SIDD_UBYTE_OPS | _SIDD_CMP_EQUAL_EACH), stringLength);
-    //if (pointOffset == 16) //|| pBegin[pointOffset] == 'e')
+    //if (pBegin[pointOffset] == 'e')
     //    return fallback(string);
 
-    // TODO: support float without "." (= int)
-
-	// Unpack epi8 => epi16
+    // Unpack epi8 => epi16
     const __m128i chars_epi16_1 = _mm_unpacklo_epi8(chars_epi8, _mm_setzero_si128());
     const __m128i chars_epi16_2 = _mm_unpackhi_epi8(chars_epi8, _mm_setzero_si128());
 
-	// Unpack epi16 => epi32
+    // Unpack epi16 => epi32
     const __m128i chars_epi32_1 = _mm_unpacklo_epi16(chars_epi16_1, _mm_setzero_si128());
     const __m128i chars_epi32_2 = _mm_unpackhi_epi16(chars_epi16_1, _mm_setzero_si128());
     const __m128i chars_epi32_3 = _mm_unpacklo_epi16(chars_epi16_2, _mm_setzero_si128());
     const __m128i chars_epi32_4 = _mm_unpackhi_epi16(chars_epi16_2, _mm_setzero_si128());
+    
+	// Slightly slower than unpacking...
+    /*const __m128i chars_epi32_1 = _mm_cvtepi8_epi32(chars_epi8);
+    const __m128i chars_epi32_2 = _mm_cvtepi8_epi32(_mm_srli_si128(chars_epi8, 4));
+    const __m128i chars_epi32_3 = _mm_cvtepi8_epi32(_mm_srli_si128(chars_epi8, 8));
+    const __m128i chars_epi32_4 = _mm_cvtepi8_epi32(_mm_srli_si128(chars_epi8, 12));*/
 
-	// Cast epi32 (int) to ps (float)
+    // Cast epi32 (int) to ps (float)
     const __m128 chars_ps_1 = _mm_cvtepi32_ps(chars_epi32_1);
     const __m128 chars_ps_2 = _mm_cvtepi32_ps(chars_epi32_2);
     const __m128 chars_ps_3 = _mm_cvtepi32_ps(chars_epi32_3);
@@ -83,24 +87,23 @@ float crack_atof_sse(std::string_view string)
     constexpr int powerLUTCenter = 16;
     static constexpr std::array<float, 32> powerLUT = computePowers(powerLUTCenter);
 
-	// Compute the sum 
-    __m128 sum = _mm_setzero_ps();
+    // Compute the sum
     const int startOffset = powerLUTCenter - pointOffset;
     const __m128 powers_1 = _mm_loadu_ps(powerLUT.data() + startOffset + 0);
-    sum = _mm_add_ps(sum, _mm_mul_ps(powers_1, chars_ps_1));
+    const __m128 sum_1 = _mm_mul_ps(powers_1, chars_ps_1);
     const __m128 powers_2 = _mm_loadu_ps(powerLUT.data() + startOffset + 4);
-    sum = _mm_add_ps(sum, _mm_mul_ps(powers_2, chars_ps_2));
+    const __m128 sum_2 = _mm_mul_ps(powers_2, chars_ps_2);
     const __m128 powers_3 = _mm_loadu_ps(powerLUT.data() + startOffset + 8);
-    sum = _mm_add_ps(sum, _mm_mul_ps(powers_3, chars_ps_3));
+    const __m128 sum_13 = _mm_fmadd_ps(powers_3, chars_ps_3, sum_1);
     const __m128 powers_4 = _mm_loadu_ps(powerLUT.data() + startOffset + 12);
-    sum = _mm_add_ps(sum, _mm_mul_ps(powers_4, chars_ps_4));
+    const __m128 sum_24 = _mm_fmadd_ps(powers_4, chars_ps_4, sum_2);
+    const __m128 sum = _mm_add_ps(sum_13, sum_24);
 
     // Horizontal sum
-    sum = _mm_hadd_ps(sum, sum);
-    sum = _mm_hadd_ps(sum, sum);
+    const __m128 hsum = _mm_hadd_ps(sum, sum);
 
     alignas(16) float sumData[4];
-    _mm_store_ps(sumData, sum);
+    _mm_store_ps(sumData, hsum);
 
-    return sign * sumData[0];
+    return sign * (sumData[0] + sumData[1]); // Final addition in scalar is faster than an extra _mm_hadd_ps()
 }
