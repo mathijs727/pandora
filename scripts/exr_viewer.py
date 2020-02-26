79ce4e8b8bb8dfd4a0134d2cdc6bd523ddb29a6e
mathijs727 <mathijs.l.molenaar@gmail.com>
2020-02-26 17:34:57 +0100

    Move EXR loading to a separate file

--- scripts/exr_viewer.py
+++ scripts/exr_viewer.py
@@ -1,34 +1,24 @@
 import argparse
-import OpenImageIO as oiio
 import matplotlib.pyplot as plt
 import numpy as np
+import os
+import shared_image
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser("EXRViewer")
 	parser.add_argument("file", type=str)
 	parser.add_argument("--max", type=float)
+	parser.add_argument("--scale", type=float, default=1)
 	args = parser.parse_args()
 
-	image = oiio.ImageInput.open(args.file)
-	spec = image.spec()
-	if spec.channelformat(0) == "uint":
-		pixels = image.read_image("UINT32")
-	else:
-		pixels = image.read_image()
-	image.close()
-
-	if pixels.shape[2] == 1:
-		plt_pixels = pixels.reshape(pixels.shape[0], -1)
-	else:
-		plt_pixels = pixels
-	
-	plt_pixels = plt_pixels / 8
+	pixels = shared_image.read_exr(args.file)
+	pixels = pixels * args.scale
 
 	if args.max:
 		vmax = args.max
 	else:
-		vmax = np.max(plt_pixels)
+		vmax = np.max(pixels)
 		print(f"Max = {vmax}")
 
-	plt.imshow(plt_pixels, vmin=0, vmax=vmax)
+	plt.imshow(pixels, vmin=0, vmax=vmax)
 	plt.show()
